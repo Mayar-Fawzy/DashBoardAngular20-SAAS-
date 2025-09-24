934937dad7c4626b4136619005dfc0a75d168a8c
Mayar-Fawzy <mayarfawzy715@gmail.com>
2025-09-25 01:17:06 +0200

    Add initial layout components and routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./Pages/Auth/login/login').then(m => m.Login)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./Pages/Auth/register/register').then(m => m.Register)
+  },
+  {
+    path: 'forget-pass',
+    loadComponent: () =>
+      import('./Pages/Auth/forget-pass/forget-pass').then(m => m.ForgetPass)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./Pages/DashBoard/dash-board/dash-board').then(m => m.DashBoard)
+  },
+  {
+    path: '**', 
+    loadComponent: () =>
+      import('./Layout/notfound/notfound').then(m => m.Notfound)
+  }
+];

f910d601687a3602b4e364b1cdd0d827688b0dac
Mayar-Fawzy <mayarfawzy715@gmail.com>
2025-09-28 23:38:57 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/Pages/DashBoard/dash-board/dash-board.ts
+++ src/app/Pages/DashBoard/dash-board/dash-board.ts
@@ -1,11 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dash-board',
-  imports: [],
+  imports: [NgClass, RouterOutlet],
   templateUrl: './dash-board.html',
   styleUrl: './dash-board.scss'
 })
 export class DashBoard {
+isCollapsed = false;
 
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
